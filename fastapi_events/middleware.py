fb98e0480b1f19346d0c678a4513857f6d9e2e82
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2021-10-19 15:13:50 +0500

    🐛 store contextvar token in closure

--- fastapi_events/middleware.py
+++ fastapi_events/middleware.py
@@ -14,27 +14,20 @@ class EventHandlerASGIMiddleware:
     def __init__(self, app: ASGIApp, handlers: Iterable[BaseEventHandler]) -> None:
         self.app = app
         self._handlers: Iterable[BaseEventHandler] = handlers
-        self._token: Optional[Token] = None
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] not in ["http", "websocket"]:
             await self.app(scope, receive, send)
             return
 
-        self._initialize_event_store()
+        token = event_store.set(deque())
         try:
             await self.app(scope, receive, send)
         finally:
             await self._process_events()
-            self._teardown_event_store()
+        event_store.reset(token)
 
     async def _process_events(self) -> None:
         q: Deque[Event] = event_store.get()
         await asyncio.gather(*[handler.handle_many(events=q)
                                for handler in self._handlers])
-
-    def _initialize_event_store(self) -> None:
-        self._token = event_store.set(deque())
-
-    def _teardown_event_store(self) -> None:
-        event_store.reset(self._token)
