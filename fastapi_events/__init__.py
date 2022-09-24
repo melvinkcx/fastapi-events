816686bbd270596dc73da1c9f1ddd9134ecfd711
melvinkcx <melvinkcx@gmail.com>
2022-09-24 15:28:51 +0900

    bump version to v0.7.0-beta

--- fastapi_events/__init__.py
+++ fastapi_events/__init__.py
@@ -4,7 +4,7 @@ from typing import Dict, Iterable
 
 from fastapi_events.handlers.base import BaseEventHandler
 
-__version__ = "0.6.0"
+__version__ = "0.7.0-beta"
 
 # handlers keeps track of all handlers registered via EventHandlerASGIMiddleware
 handler_store: Dict[int, Iterable[BaseEventHandler]] = defaultdict(list)
