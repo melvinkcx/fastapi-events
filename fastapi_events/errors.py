0df3db27dde7e18b19a92c20e011298dea690437
melvin <melvinkcx@gmail.com>
2024-01-06 10:58:22 -0500

    add new test suite; support implicit event name

--- fastapi_events/errors.py
+++ fastapi_events/errors.py
@@ -4,3 +4,12 @@ class FastapiEventError(BaseException):
 
 class ConfigurationError(FastapiEventError):
     pass
+
+
+class MissingEventNameDuringRegistration(FastapiEventError):
+    def __init__(self):
+        super().__init__(
+            "Event name cannot be determined during schema registration. "
+            "Please ensure '__event_name__' is defined in your payload schema "
+            "or 'event_name' is provided to @payload_schema.register() as a kwarg."
+        )
